6dbb21a48217194b18f7048e30777757dbbf19ea
RUBY NOHELIA <zafirorachelrn@gmail.com>
2022-05-26 17:14:36 +0000

    Kaworu

--- EstRepetitivas/Ejerciciowip.java
+++ EstRepetitivas/Ejerciciowip.java
@@ -12,13 +12,13 @@ public class Ejerciciowip {
         System.out.println("Ingrese num fila para Linea:");
         int numFilLinea=leerT.nextInt();
         int medio=cantF+1;
-         for (int f = 1; f < cantF; f++) {
-             for (int c = 1; c <= 2*cantF; c++) {
+         for (int f = 1; f <= cantF; f++) {
+             for (int c = 1; c <= 2*cantF+1; c++) {
                  if (f==1 && medio==c) {
                      System.out.print("*");   
-                 }else if (c==medio-1 && f>1) {
+                 }else if (c==medio-f && f>1) {
                      System.out.print("*");
-                 }else if(c==medio+1 && f>1){
+                 }else if(c==medio+f && f>1){
                      System.out.print("*");
                  }else if(f==numFilLinea && c>medio+1 && c<medio+1 ){
                      System.out.print("*");
